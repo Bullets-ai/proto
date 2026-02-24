2038e8300e7cf72b80f584e7e67d5e08a5583fba
Jungun Lee <julee@Jungunui-noteubug.local>
2026-02-24 20:33:59 +0900

    Add proto-file

---
+++ gptprompt_final.py
@@ -0,0 +1,13 @@
+{
+	"folders": [
+		{
+			"path": "Bullets/bullets-app"
+		}
+	],
+	"settings": {
+		"terminal.integrated.env.osx": {
+			"PATH": "${workspaceFolder}/.venv/bin:${env:PATH}",
+			"PYTHONPATH": "${workspaceFolder}"
+		}
+	}
+}
